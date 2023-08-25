dbda1c45575ffc7e7eb8e43618df6e3e4624056c
Venumadhav Vavilala <124782475+MrNotime00@users.noreply.github.com>
2023-08-25 15:39:17 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+      name: "Take Me Away",
+      artist: "New Medicine",
+      img: "music-1",
+      src: "music-1" 
+    },
+    {
+      name: "Don't-Let-Break-Your-Heart",
+      artist: " Louis Tomlinson",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Runaway",
+      artist: "Aurora",
+      img: "music-3",
+      src: "music-3"
+    },
+  ];
